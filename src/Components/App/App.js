21349e40f504f99b1b56b1f2b79788b29d090335
Kristy Nguyen <kristypnguyen7@gmail.com>
2022-08-04 15:05:50 -0600

    Add redirect for misspelled URL

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch, Redirect } from 'react-router-dom';
 import ShopsDisplay from '../ShopsDisplay/ShopsDisplay';
 import RegionChoices from '../RegionChoices/RegionChoices';
 import { getShops } from '../../apiCalls';
@@ -33,26 +33,33 @@ const App = () => {
             </Link>
           </nav>
           <main>
-            <Route 
-              exact path='/' render={() => (
-                <section className='home'>
-                  <section className='about'>
-                    <p className='description'>Welcome to Boba Buddies!</p>
+            <Switch>
+              <Route 
+                exact path='/' render={() => (
+                  <section className='home'>
+                    <section className='about'>
+                      <p className='description'>Welcome to Boba Buddies!</p>
+                    </section>
+                    <RegionChoices />
                   </section>
-                  <RegionChoices />
-                </section>
-              )} 
-            />
-            <Route 
-              exact path='/shops/:region' render={( {match} ) => {
-                if (match.params.region === 'All') {
-                  return <ShopsDisplay filteredShops={shops} region={match.params.region} error={error} />
-                } else {
-                  const shopsToRender = shops.filter(shop => shop.region === match.params.region);
-                  return <ShopsDisplay filteredShops={shopsToRender} region={match.params.region} error={error} />
-                }
-              }}
-            />
+                )} 
+              />
+              <Route 
+                path='/shops/:region' render={( {match} ) => {
+                  if (match.params.region === 'All') {
+                    return <ShopsDisplay filteredShops={shops} region={match.params.region} error={error} />
+                  } else {
+                    const shopsToRender = shops.filter(shop => shop.region === match.params.region);
+                    return <ShopsDisplay filteredShops={shopsToRender} region={match.params.region} error={error} />
+                  }
+                }}
+              />
+              <Route 
+                render={() => 
+                  <Redirect to={{pathname: "/"}} />
+                } 
+              />
+            </Switch>
           </main>
         </>
       }
