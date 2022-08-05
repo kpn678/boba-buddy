2ffd7ee3f5e0096d1e9330caa66db33b1924ad7b
Kristy Nguyen <kristypnguyen7@gmail.com>
2022-08-04 23:04:35 -0600

    Convert links to <a> tag

--- src/Components/ShopCard/ShopCard.js
+++ src/Components/ShopCard/ShopCard.js
@@ -2,12 +2,17 @@ import React from "react";
 import PropTypes from 'prop-types';
 import './ShopCard.css';
 
-const ShopCard = ({ id, name, address, website }) => {
+const ShopCard = ({ id, name, address, yelp, facebook, instagram, website }) => {
   return (
     <section className='individual-card'>
       <h3>{name}</h3>
       <p className='address'>{address}</p>
-      <p className='website'>{website}</p>
+      <div className='links-container'>
+        <a className='yelp' href={yelp} target='_blank' rel='noopener'>Yelp</a>
+        <a className='facebook' href={facebook} target='_blank' rel='noopener'>Facebook</a>
+        <a className='instagram' href={instagram} target='_blank' rel='noopener'>Instagram</a>
+        <a className='website' href={website} target='_blank' rel='noopener'>Website</a>
+      </div>
     </section>
   );
 };
@@ -20,5 +25,8 @@ ShopCard.propTypes = {
   id: number.isRequired,
   name: string.isRequired,
   address: string.isRequired,
+  yelp: string.isRequired,
+  facebook: string,
+  instagram: string,
   website: string
 };
