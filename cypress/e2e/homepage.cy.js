a03cc6c98895ff04a965163f0fb517b39a7a61ce
Kristy Nguyen <kristypnguyen7@gmail.com>
2022-08-05 11:55:21 -0600

    Test region selection section

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -12,7 +12,17 @@ describe('Visit homepage', () => {
   });
 
   it('Should display a description of the page', () => {
-    cy.get('.description').contains('Welcome to Boba Buddy!')
+    cy.get('.description').contains('Welcome to Boba Buddy!');
   });
 
+  it('Should display a section with a list of different regions to pick from', () => {
+    cy.get('.regions-header').contains('Denver Metro Area Regions:');
+    cy.get('.region-choices').should('be.visible');
+    cy.get('.region-btn').should('have.length', 8);
+  });
+
+  it('Should navigate to specific region page once button is clicked', () => {
+    cy.get('.region-btn').eq(4).click();
+    cy.url().should('eq', 'http://localhost:3000/shops/Central');
+  });
 });
