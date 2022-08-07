e083fd4c4c1c8bdbf427803a4895a0cc794dd3f9
Kristy Nguyen <kristypnguyen7@gmail.com>
2022-08-07 13:14:43 -0600

    Display searched shops on homepage

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -31,6 +31,10 @@ const App = () => {
     setSearchedShops(shops.filter(shop => shop.name.toUpperCase().includes(event.target.value.toUpperCase())));
   };
 
+ const resetSearch = () => {
+  setSearchValue('');
+ };
+
   return (
     <>
       {error ? <h2 className='error-message'>{error}</h2> : 
@@ -45,21 +49,27 @@ const App = () => {
             {shops.length === 0 ? <h2 className='loading-message'>Page Loading...</h2> : 
               <Switch>
                 <Route 
-                  exact path='/' render={() => (
-                    <section className='home'>
-                      <section className='about'>
-                        <h3 className='tagline'>Welcome to Boba Buddy, your guide to boba and milk tea in the Denver Metro Area!</h3>
-                        <p className='description'> 
-                          Prior to 2017, there were only about five boba shops in the metro area, give or take. Now, there are well over
-                          60 shops in the area! Even with the abundance of shops, it can be hard to locate a shop and its information or
-                          find relevant results. This guide is designed to be a comprehensive (or as close as possible to comprehensive) 
-                          list of all shops that have a storefront and feature boba or milk tea as a predominant part of their menu. 
-                          What are you waiting for, go get some boba or tea!
-                        </p>
-                      </section>
-                      <RegionChoices />
-                    </section>
-                  )} 
+                  exact path='/' render={() => {
+                    if (searchedShops.length !== 0) {
+                      return <ShopsDisplay filteredShops={searchedShops} error={error} />
+                    } else {
+                      return (
+                        <section className='home'>
+                          <section className='about'>
+                            <h3 className='tagline'>Welcome to Boba Buddy, your guide to boba and milk tea in the Denver Metro Area!</h3>
+                            <p className='description'> 
+                              Prior to 2017, there were only about five boba shops in the metro area, give or take. Now, there are well over
+                              60 shops in the area! Even with the abundance of shops, it can be hard to locate a shop and its information or
+                              find relevant results. This guide is designed to be a comprehensive (or as close as possible to comprehensive) 
+                              list of all shops that have a storefront and feature boba or milk tea as a predominant part of their menu. 
+                              What are you waiting for, go get some boba or tea!
+                            </p>
+                          </section>
+                          <RegionChoices />
+                        </section>
+                      );
+                    };
+                  }} 
                 />
                 <Route 
                   path='/shops/:region' render={( {match} ) => {
