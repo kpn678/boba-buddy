d17b6d776e9b75f277687283278f1e12064883e9
Kristy Nguyen <kristypnguyen7@gmail.com>
2022-08-07 15:13:51 -0600

    Rewrite description message

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -60,9 +60,10 @@ const App = (props) => {
                           <section className='about'>
                             <h3 className='tagline'>Welcome to Boba Buddy, your guide to boba and milk tea in the Denver Metro Area!</h3>
                             <p className='description'> 
-                              Prior to 2017, there were only about five boba shops in the metro area, give or take. Now, there are well over
-                              60 shops in the area! Even with the abundance of shops, it can be hard to locate a shop and its information or
-                              find relevant results. This guide is designed to be a comprehensive (or as close as possible to comprehensive) 
+                              Prior to 2017, there were only about five boba shops in the metro area, give or take. Now, there are almost
+                              70 shops in the area! Even with the abundance of shops, it can be hard to locate a shop and its information or
+                              find relevant results.<br></br>
+                              This guide is designed to be a comprehensive (or as close as possible to comprehensive) 
                               list of all shops that have a storefront and feature boba or milk tea as a predominant part of their menu. 
                               What are you waiting for, go get some boba or tea!
                             </p>
